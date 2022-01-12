fdaa97b7c0fa02211adff6e968aed9a8e88c75e1
Silivastruflorin Florin <31577537+silivastruflorin@users.noreply.github.com>
2022-01-12 22:37:40 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App ex
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters : [],  
+    };
+  }
+
+ 
+  componentDidMount(){
+    console.log("Component was mounted");
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((users) => this.setState({monsters:users},() => console.log(this.state.monsters)));
+    
+  }
+
+  componentDidUpdate(){
+    console.log("State was updated");
+  }
+
+  render(){
+    // console.log(this.state.monsters)
+    console.log("render was called " + new Date().toTimeString());
+    return(
+      <div>
+        {
+          this.state.monsters.map(elementInMonsters =>
+                                              (
+                                                <h1 id={elementInMonsters.id}>{elementInMonsters.name}</h1>
+                                              )
+                                 ) 
+        }
+      
+      </div>
+    );
+  }
+}
 export default App;
+
+/*
+    Render method is called 
+    then ComponentDidMount is called and updates the state 
+    beacause state was changed the render method is called again 
+    and after that ComponentDidUpdate is called.
+
+    at the first render monsters array is empty 
+    and by using map(i think has try catch inside)
+    does not trow error but if we use directly <div>{ this.state.monsters[0]}</div>
+    will trow error.
+*/
